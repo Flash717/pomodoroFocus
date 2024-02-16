6c619d8e884ab8b201bd61779ac0517694484c51
Florian Knaus <florian.knaus@gmail.com>
2024-02-16 12:28:14 -0600

    Initial Commit

---
+++ PomodoroFocus/PomodoroFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroFocusApp.swift
+//  PomodoroFocus
+//
+//  Created by Florian Knaus on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
